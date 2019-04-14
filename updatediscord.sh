71d76119f23e7c28a83185958e758899ab82b1ab
Private-Locker, LLC <40194049+PrivateLocker@users.noreply.github.com>
2019-04-14 07:08:47 -0700

    Update updatediscord.sh

--- updatediscord.sh
+++ updatediscord.sh
@@ -2,17 +2,26 @@
 name="Discord"
 tempname="discord.deb"
 updateurl="https://discordapp.com/api/download?platform=linux&format=deb"
-
+clear;
+echo -e "Killing all Processes with name $name.";
 # Killing all Processes using Discord name.
 for KILLPID in `ps ax | grep $name | awk ' { print $1;}'`; do 
-  kill -9 $KILLPID;
+  kill -9 $KILLPID &> /dev/null
 done
-
+echo -e "";
+echo -e "Pulling $tempname from $updateurl";
 # Pulling current version from Discord.com
-wget "$updateurl" -O $pwd/$tempname
-
+wget "$updateurl" -O $pwd/$tempname >/dev/null 2>&1
+echo -e "";
+echo -e "Installing $tempname";
 # Installing current version of Discord
-dpkg -i $pwd/$tempname
-
+dpkg -i $pwd/$tempname >/dev/null
+echo -e "";
+echo -e "Removing temp $tempname";
 # Removing temp .deb file
-rm -rf $tempname
+rm -rf $tempname >/dev/null
+echo -e "Finished"
+echo -e "";
+echo -e "Launching $name"
+echo -e "";
+discord  & >/dev/null 2>&1
